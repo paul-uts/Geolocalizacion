6149a4c5fd9c27e106882f77ca209dc2dd43e6ac
Paúl Ernesto López Peña <pelp.uts@gmail.com>
2026-02-07 11:08:42 -0700

    Create GeolocalizacionModel.cs
    GeolocalizacionModel

---
+++ Geolocalizacion/Models/GeolocalizacionModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Geolocalizacion.Models
+{
+    public class GeolocalizacionModel
+    {
+        public string status { get; set; }
+        public string country { get; set; }
+        public string regionName { get; set; }
+        public string city { get; set; }
+        public float lat { get; set; }
+        public float lon { get; set; }
+        public string query { get; set; }
+    }
+}
