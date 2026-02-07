a4f85f0d3e0aeabe08d52a3222457c2d98e3eca2
AlexEduardo04709 <edualexan047@gmail.com>
2026-02-07 14:07:20 -0700

    GeolocalizacionController

---
+++ Geolocalizacion/Controllers/GeolocalizacionController.cs
@@ -0,0 +1,34 @@
+﻿using Geolocalizacion.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geolocalizacion.Controllers
+{
+    public class GeolocalizacionController : Controller
+    {
+        private readonly GeolocalizacionService _service;
+
+        public GeolocalizacionController()
+        {
+            _service = new GeolocalizacionService();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string ip)
+        {
+            if (string.IsNullOrEmpty(ip))
+            {
+                ViewBag.Error = "Ingresa una IP válida";
+                return View();
+            }
+
+            var resultado = await _service.ObtenerPorIp(ip);
+
+            return View(resultado);
+        }
+    }
+}
